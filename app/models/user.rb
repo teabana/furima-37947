409fd2ab8d7fb1dded0e10ace0b2ec145af6c716
teabana <micky59322@gmail.com>
2022-05-17 20:04:36 +0900

    passwordのバリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    # 半角英数字のみ許可する
-    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input a mixture of single-byte alphanumeric characters."}
     # ひらがな、カタカナ、漢字のみ許可する
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' } do
       validates :last_name
@@ -20,4 +18,7 @@ class User < ApplicationRecord
     end
     validates :birthday
   end
+  # 半角英数字のみ許可する
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input a mixture of single-byte alphanumeric characters."}
+
 end
