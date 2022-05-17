aca17cdc68cb0d4be55558ee9c8785e3dd704fb5
teabana <micky59322@gmail.com>
2022-05-17 21:31:24 +0900

    出品ページ遷移実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
+    
   end
+
+  def new
+    # @product = product.new
+  end
+
+  def create
+  end
+
 end
