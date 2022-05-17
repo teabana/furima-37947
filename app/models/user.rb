ef2bc178a9fc3cc851a6f4ea90cc409c84a02dec
teabana <micky59322@gmail.com>
2022-05-17 15:45:47 +0900

    deviseの導入、validates設定

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    # ひらがな、カタカナ、漢字のみ許可する
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."} do
+      validates :last_name
+      validates :first_name
+    end
+    # カタカナのみ許可する
+    with_options format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    validates :birthday
+  end
+
+end
