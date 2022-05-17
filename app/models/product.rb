8e9b558c0d090b4695b5642494174095cbf1b1f5
teabana <micky59322@gmail.com>
2022-05-18 08:38:57 +0900

    status_collectionの実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
 
   belongs_to :user
   has_one_attached :image
@@ -10,7 +11,7 @@ class Product < ApplicationRecord
     validates     :price
     validates     :detail
     validates     :category_id,  numericality: { other_than: 0, message: "can't be blank "}
-    validates     :status_id
+    validates     :status_id,  numericality: { other_than: 0, message: "can'T be blank"}
     validates     :shipping_cost_id
     validates     :prefecture_id
     validates     :shipping_day_id
