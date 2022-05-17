2d3f5b0a631303ee7be559bb0885383cf6d4b7e0
teabana <micky59322@gmail.com>
2022-05-18 08:47:52 +0900

    shipping_cost_collectionの実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :shipping_cost
 
   belongs_to :user
   has_one_attached :image
@@ -11,9 +12,9 @@ class Product < ApplicationRecord
     validates     :price
     validates     :detail
     validates     :category_id,  numericality: { other_than: 0, message: "can't be blank "}
-    validates     :status_id,  numericality: { other_than: 0, message: "can'T be blank"}
-    validates     :shipping_cost_id
-    validates     :prefecture_id
+    validates     :status_id,  numericality: { other_than: 0, message: "can't be blank"}
+    validates     :shipping_cost_id, numericality: { other_than: 0, message: "can't be blank"}
+    validates     :prefecture_id 
     validates     :shipping_day_id
   end
 end
