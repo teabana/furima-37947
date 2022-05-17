514ad3cf44292646faf998c7db57bfc1abb6049a
teabana <micky59322@gmail.com>
2022-05-18 07:13:00 +0900

    商品のバリデーション実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+
+
+  with_options presence: true do
+    validates     :name
+    validates     :price
+    validates     :detail
+    validates     :category_id
+    validates     :status_id
+    validates     :shipping_cost_id
+    validates     :prefecture_id
+    validates     :shipping_day_id
+  end
 end
