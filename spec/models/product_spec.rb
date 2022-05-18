ee0297e50bb20382398d8b13637ab35c64af7208
teabana <micky59322@gmail.com>
2022-05-18 14:48:00 +0900

    商品出品機能(レビュー後修正一回目)

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -28,28 +28,28 @@ RSpec.describe Product, type: :model do
         @product.valid?
         expect(@product.errors.full_messages).to include("Detail can't be blank")
       end
-      it 'category_idが空では出品できない' do
-        @product.category_id = ''
+      it 'category_idが0では出品できない' do
+        @product.category_id = 0
         @product.valid?
         expect(@product.errors.full_messages).to include("Category can't be blank")
       end
-      it 'status_idが空では出品できない' do
-        @product.status_id = ''
+      it 'status_idが0では出品できない' do
+        @product.status_id = 0
         @product.valid?
         expect(@product.errors.full_messages).to include("Status can't be blank")
       end
-      it 'shipping_cost_idが空では出品できない' do
-        @product.shipping_cost_id = ''
+      it 'shipping_cost_idが0では出品できない' do
+        @product.shipping_cost_id = 0
         @product.valid?
         expect(@product.errors.full_messages).to include("Shipping cost can't be blank")
       end
-      it 'prefecture_idが空では出品できない' do
-        @product.prefecture_id = ''
+      it 'prefecture_idが0では出品できない' do
+        @product.prefecture_id = 0
         @product.valid?
         expect(@product.errors.full_messages).to include("Prefecture can't be blank")
       end
-      it 'shipping_day_idが空では出品できない' do
-        @product.shipping_day_id = ''
+      it 'shipping_day_idが0では出品できない' do
+        @product.shipping_day_id = 0
         @product.valid?
         expect(@product.errors.full_messages).to include("Shipping day can't be blank")
       end
@@ -72,6 +72,11 @@ RSpec.describe Product, type: :model do
         @product.price = '９９９９'
         @product.valid?
         expect(@product.errors.full_messages).to include('Price is invalid. Input half-width characters')
+      end
+      it 'ユーザ情報がなけれな出品できない' do
+        @product.user = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("User must exist")
       end
     end
   end
