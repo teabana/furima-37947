d9dba5856e75521d53bb7b461edf53ba572ac473
teabana <micky59322@gmail.com>
2022-05-18 09:15:07 +0900

    shipping_day_collectionの実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   belongs_to :status
   belongs_to :shipping_cost
   belongs_to :prefecture
+  belongs_to :shipping_day
 
   belongs_to :user
   has_one_attached :image
@@ -16,6 +17,6 @@ class Product < ApplicationRecord
     validates     :status_id,  numericality: { other_than: 0, message: "can't be blank"}
     validates     :shipping_cost_id, numericality: { other_than: 0, message: "can't be blank"}
     validates     :prefecture_id, numericality: { other_than: 0, message: "can't be blank"} 
-    validates     :shipping_day_id
+    validates     :shipping_day_id, numericality: { other_than: 0, message: "can't be blank"}
   end
 end
