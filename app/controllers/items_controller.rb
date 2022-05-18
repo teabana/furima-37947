132729294beb2a1b669de4b180aa6161ddfda576
teabana <micky59322@gmail.com>
2022-05-18 18:25:22 +0900

    編集保存の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_product, only: [:show, :edit, :update]
 
   def index
     @products = Product.all.order(created_at: 'DESC')
@@ -19,21 +20,28 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
+    if @product.update(product_params)
+      redirect_to     item_path
+    else
+      render :edit
+    end
   end
 
-  
+
   private
 
   def product_params
     params.require(:product).permit(:name, :price, :detail, :category_id, :status_id, :shipping_cost_id, :prefecture_id,
                                     :shipping_day_id, :image).merge(user_id: current_user.id)
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
   end
 end
