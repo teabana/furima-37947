1a02cd4482e02405715a7d783619d2bd643e9c58
teabana <micky59322@gmail.com>
2022-05-18 09:23:12 +0900

    priceバリデーションの実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,9 +11,11 @@ class Product < ApplicationRecord
 
   with_options presence: true do
     validates     :name
-    validates     :price
+    validates     :price,  numericality: {
+      only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"
+    }
     validates     :detail
-    validates     :category_id,  numericality: { other_than: 0, message: "can't be blank "}
+    validates     :category_id,  numericality: { other_than: 0, message: "can't be blank"}
     validates     :status_id,  numericality: { other_than: 0, message: "can't be blank"}
     validates     :shipping_cost_id, numericality: { other_than: 0, message: "can't be blank"}
     validates     :prefecture_id, numericality: { other_than: 0, message: "can't be blank"} 
