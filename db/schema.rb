e7fb555332412862e769c3268e57899371865232
teabana <micky59322@gmail.com>
2022-05-19 12:34:41 +0900

    配送先テーブルの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_18_071808) do
+ActiveRecord::Schema.define(version: 2022_05_19_032849) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 2022_05_18_071808) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "prefecrure_id", null: false
+    t.string "city", null: false
+    t.string "blockname", null: false
+    t.string "building"
+    t.string "phone_number", null: false
+    t.bigint "purchase_order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["purchase_order_id"], name: "index_destinations_on_purchase_order_id"
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -76,6 +89,7 @@ ActiveRecord::Schema.define(version: 2022_05_18_071808) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "destinations", "purchase_orders"
   add_foreign_key "products", "users"
   add_foreign_key "purchase_orders", "products"
   add_foreign_key "purchase_orders", "users"
